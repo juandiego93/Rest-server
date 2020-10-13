6484d508741ade0cdbdee59ec94123b3a0492360
Juan Diego Ariadna <diego.osorio@ariadnacg.com>
2020-10-13 11:37:25 -0500

    First comiit

---
+++ server/server.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const app = express()
+require('./config/config')
+
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.get('/', (request, response) => {
+    response.json('Hola mundo')
+})
+
+app.get('/usuario', (request, response) => {
+    response.json('Hola mundo')
+})
+
+app.post('/usuario', (request, response) => {
+    let body = request.body
+    if (body.nombre === undefined) {
+        response.status(400).json({
+            ok: false,
+            message: 'El nombre es necesario'
+        })
+    } else {
+        response.json({ body })
+    }
+})
+
+app.put('/usuario/:id', (request, response) => {
+    let id = request.params.id
+    response.json({ id })
+})
+
+app.delete('/usuario', (request, response) => {
+    response.json('Hola mundo')
+})
+
+app.listen(process.env.PORT, () => {
+    console.log('Server running:', port);
+})
